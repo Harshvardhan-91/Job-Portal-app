a1ccc72f19fbd252f04edb6edde5d2b09cd8c773
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-09-18 02:10:34 +0530

    Completed Home Page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './shared/Navbar';
+import HeroSection from "../components/HeroSection";
+import CategoryCarousel from './CategoryCarousel';
+import LatestJobs from './LatestJobs';
+import Footer from './shared/Footer';
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <HeroSection/>
+        <CategoryCarousel/>
+        <LatestJobs/>
+        <Footer/>
+        
+
     </div>
   )
 }
